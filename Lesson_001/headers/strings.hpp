e33baf1eca6f2426500ec2888392be59f83f507b
Nico Greco <ncgreco1440@gmail.com>
2019-11-25 16:31:36 -0500

    formatting

--- Lesson_001/headers/strings.hpp
+++ Lesson_001/headers/strings.hpp
@@ -3,11 +3,11 @@
 std::string printName(const std::string first, const std::string last);
 
 std::string printName(const std::string first, const std::string last,
-    const std::string middle);
+  const std::string middle);
 
 unsigned int nameLength(const std::string first);
 
 unsigned int nameLength(const std::string first, const std::string last);
 
 unsigned int nameLength(const std::string first, const std::string last,
-    const std::string middle);
+  const std::string middle);
--- Lesson_001/src/strings.cpp
+++ Lesson_001/src/strings.cpp
@@ -1,23 +1,23 @@
 #include <string>
 
 std::string printName(const std::string first, const std::string last) {
-    return last + ", " + first;
+  return last + ", " + first;
 }
 
 std::string printName(const std::string first, const std::string last,
-    const std::string middle) {
-    return last + ", " + first + " " + middle + ".";
+  const std::string middle) {
+  return last + ", " + first + " " + middle + ".";
 }
 
 unsigned int nameLength(const std::string first) {
-    return first.length();
+  return first.length();
 }
 
 unsigned int nameLength(const std::string first, const std::string last) {
-    return first.length() + last.length();
+  return first.length() + last.length();
 }
 
 unsigned int nameLength(const std::string first, const std::string last,
-    const std::string middle) {
-    return first.length() + last.length() + middle.length();
+  const std::string middle) {
+  return first.length() + last.length() + middle.length();
 }
