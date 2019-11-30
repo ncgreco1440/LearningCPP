8dc1f63fd240efc820fa4e7ca644d710bf3a6d93
Nico Greco <ncgreco1440@gmail.com>
2019-11-30 11:56:23 -0500

    comments to enums

--- Lesson_003/include/status.hpp
+++ Lesson_003/include/status.hpp
@@ -1,19 +1,54 @@
 #ifndef STATUS_H_
 #define STATUS_H_
 
+#include <string>
+
 namespace LCPP {
 
-  enum Network {
+  /**
+   * C++11/17 allows you to specify the underlying type of the enum,
+   * the type MUST still be an integral data type
+   * int
+   * short
+   * long
+   * char
+   * 
+   * unsigned combinations are available too
+   * 
+   * @example
+   * enum class MyEnum: [integral data type] {
+   *  enum1,
+   *  enum2
+   * }
+   */
+
+  enum class Network: unsigned int {
     OK,
     PENDING,
     ERROR
   };
 
-  enum class FileOperation {
-    READ = 'read',
-    WRITE = 'write',
-    READ_WRITE = 'read/write',
-    COPY = 'copy'
+  enum class FileOperation: unsigned char {
+    READ,
+    WRITE,
+    READ_WRITE,
+    COPY
+  };
+
+  enum class HTTP_STATUS: unsigned short {
+    OK = 200,
+    MOVED_PERMANENTLY = 301,
+    FOUND = 302,
+    NOT_MODIFIED = 304,
+    NOT_FOUND = 404,
+    METHOD_NOT_ALLOWED = 405,
+    NOT_ACCEPTABLE = 406,
+    PROXY_AUTH_REQUIRED = 407,
+    INTERNAL_SERVER_ERROR = 500,
+    NOT_IMPL = 501,
+    BAD_GATEWAY = 502,
+    SERVER_UNAVAIL = 503,
+    GATEWAY_TIMEOUT = 504
   };
 
 }
