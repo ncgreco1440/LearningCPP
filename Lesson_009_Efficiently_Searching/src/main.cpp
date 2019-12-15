0fd5158eb3c7bd296229312066724a1140fdecf7
Nico Greco <ncgreco1440@gmail.com>
2019-12-15 14:37:37 -0500

    unhandled exceptions

--- Lesson_009_Efficiently_Searching/src/main.cpp
+++ Lesson_009_Efficiently_Searching/src/main.cpp
@@ -1,3 +1,22 @@
+#include <exception>
+#include <iostream>
+
+namespace OT {
+
+  /**
+   * Function to invoke when an exception is thrown and unhandled
+   * @return void
+   */
+  void unhandled() {
+    std::cout << "Unhandled Exception\n";
+    std::abort();
+  }
+
+} // namespace OT
+
 int main() {
+  std::set_terminate(OT::unhandled);
+  std::cout << "Hello world from MSVC Compiler!\n";
+  throw 6;
   return 0;
 }
